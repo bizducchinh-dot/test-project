8772e7b9dee0730bb07d4298f1b461e266c653d5
bizducchinh-dot <bizducchinh@gmail.com>
2026-04-21 15:20:16 +0700

    feat: add en zod - directory_v1

--- src/mcp-tool/tools/en/gen-tools/zod/directory_v1.ts
+++ src/mcp-tool/tools/en/gen-tools/zod/directory_v1.ts
@@ -21,3 +21,44 @@ export type directoryV1ToolName =
   | 'directory.v1.employee.resurrect'
   | 'directory.v1.employee.search'
   | 'directory.v1.employee.toBeResigned';
+export const directoryV1CollaborationRuleCreate = {
+  project: 'directory',
+  name: 'directory.v1.collaborationRule.create',
+  sdkName: 'directory.v1.collaborationRule.create',
+  path: '/open-apis/directory/v1/collaboration_rules',
+  httpMethod: 'POST',
+  description:
+    '[Feishu/Lark]-Trust Party-Collaboration rules-Add collaboration rules-Admin perspective Added collaboration rules. Users need to have trusted party administrator role',
+  accessTokens: ['tenant', 'user'],
+  schema: {
+    data: z.object({
+      subjects: z
+        .object({
+          open_user_ids: z
+            .array(z.string())
+            .describe('User open id, which can be obtained from our Contacts/organizational structure interfaces')
+            .optional(),
+          open_department_ids: z
+            .array(z.string())
+            .describe(
+              'Department open id, 0 represents all members, which can be obtained from our Contacts/organizational structure interfaces',
+            )
+            .optional(),
+          open_group_ids: z
+            .array(z.string())
+            .describe('The user group open id can be obtained from our Contacts/organizational structure interfaces')
+            .optional(),
+        })
+        .describe('The sum of the number of entities needs to be less than 100'),
+      objects: z
+        .object({
+          open_user_ids: z.array(z.string()).describe('User open id').optional(),
+          open_department_ids: z.array(z.string()).describe('Department open id, 0 represents all members').optional(),
+          open_group_ids: z.array(z.string()).describe('Group open id').optional(),
+        })
+        .describe('The sum of the number of entities needs to be less than 100'),
+    }),
+    params: z.object({ target_tenant_key: z.string().describe("The other tenant's tenant key") }),
+    useUAT: z.boolean().describe('Use user access token, otherwise use tenant access token').optional(),
+  },
+};
